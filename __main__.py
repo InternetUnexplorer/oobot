d1811f43bedac2f0222d475fe1ed2fb9fb5bd4fe
InternetUnexplorer <internetunexplorer@gmail.com>
2022-01-31 17:09:39 -0800

    Add reply feature

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import random
 from asyncio import sleep
 from os import environ
+from typing import Optional
 
 from discord import Client, Status, Game, TextChannel, Message
 
@@ -10,30 +11,29 @@ class OobClient(Client):
         super().__init__(**options)
         self.channel_id = channel_id
 
-    async def oob(self) -> None:
-        channel: TextChannel = self.get_channel(self.channel_id)
+    async def oob(self, message: Optional[Message]) -> None:
+        channel: TextChannel = message.channel if message else self.get_channel(self.channel_id)
         with channel.typing():
             await sleep(random.randrange(5))
-            await channel.send("oob")
+            if message:
+                await message.reply("oob")
+            else:
+                await channel.send("oob")
 
     async def on_ready(self) -> None:
         print(f"Logged in as {self.user}!")
         await self.change_presence(status=Status.idle, activity=Game("oob"))
-        await self.oob()
 
     async def on_message(self, message: Message) -> None:
         # Don't respond too our own oobs (infinite oob loop!)
         if message.author == self.user:
             return
-        # Don't respond if the message is outside the oob channel
-        if message.channel.id != self.channel_id:
-            return
-        # Don't respond if the message is not oob (heresy!)
-        if message.content != "oob":
-            return
-        # Otherwise, oob!
-        await sleep(random.randrange(5))
-        await self.oob()
+        # Respond if the message is in the oob channel or if we were mentioned
+        is_mention = self.user.mentioned_in(message)
+        is_in_oob_channel = message.channel.id == self.channel_id
+        if is_mention or is_in_oob_channel:
+            await sleep(random.randrange(5))
+            await self.oob(None if not is_mention else message)
 
 
 if __name__ == "__main__":
